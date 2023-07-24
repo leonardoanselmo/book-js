2205e57f7578061acfa18137d97833896af09ced
Léo Ansélmo <leonardo_anselmo@hotmail.com>
2023-07-24 20:29:45 -0300

    Números e operadores

--- js/numeros-op.js
+++ js/numeros-op.js
@@ -20,3 +20,36 @@ console.log(subtracao);
 console.log(multiplicacao);
 console.log(divisao);
 
+var soma = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+console.log(soma);
+
+var incremento = 5;
+console.log(++incremento);
+console.log(incremento);
+
+console.log(--incremento);
+
+var idade = '28';
+var somaIdade = 5;
+
+// Transformou a variável idade em número e somou com somaIdade.
+console.log(+idade + somaIdade);
+
+// Resultado será NaN => Não é um número.
+var expressao1 = 'teste' / 2;
+var expressao2 = 'teste' * 2;
+console.log(expressao1, expressao2);
+
+var somatorio = +'200' + 50;
+console.log(somatorio);
+
+var x = 5;
+x++;
+console.log(x);
+
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade;
+
+
+console.log(peso);
