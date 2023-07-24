fb69bf9dcc14845dd7e7223e5b6f9fd2913e642b
Léo Ansélmo <leonardo_anselmo@hotmail.com>
2023-07-24 17:57:50 -0300

    Tipos de dados em JS

---
+++ js/tipos-dados.js
@@ -0,0 +1,26 @@
+var sobrenome = 'Ansélmo';
+var nome; 
+var idade = 44;
+var hotel = {};
+var time = null;
+var simbolo = Symbol();
+
+console.log(typeof sobrenome, typeof nome, typeof idade, typeof hotel);
+console.log(typeof time);
+console.log(typeof simbolo);
+
+var gols = 1000;
+var frase = `Romário fez ${gols} gols`;
+console.log(frase);
+
+// Exercícios 
+var objeto = 'computador';
+var dataFabricacao = '1998';
+var minhaIdade = 44;
+var meuNome = 'Léo';
+var meuSobrenome = 'Ansélmo';
+var nomeCompleto = `${meuNome} ${meuSobrenome}`;
+var fraseIngles = "It´s time";
+
+console.log(nomeCompleto);
+console.log(typeof nomeCompleto);
