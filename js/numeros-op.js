99e8d4ac391bf1087d3c21027952ccbd31bd3e83
Léo Ansélmo <leonardo_anselmo@hotmail.com>
2023-07-24 20:09:45 -0300

    Números e operadores

---
+++ js/numeros-op.js
@@ -0,0 +1,22 @@
+var idade = 44;
+var pi = 3.14;
+var exp=2e10;
+
+console.log(idade);
+console.log(pi);
+console.log(exp);
+
+var expoente = 2**4;
+var modulo = 10 % 5;
+
+console.log(expoente);
+console.log(modulo);
+
+var subtracao = '100' - 70;
+var multiplicacao = '150' * 2;
+var divisao = '100' / 2;
+
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
