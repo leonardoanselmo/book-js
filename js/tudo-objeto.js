e948348bf0ddc2af850574f6150d4ac7ac7c4077
Léo Ansélmo <leonardo_anselmo@hotmail.com>
2023-07-26 09:18:41 -0300

    Tudo é objeto

---
+++ js/tudo-objeto.js
@@ -0,0 +1,13 @@
+var nome = 'Léo Anselmo';
+
+console.log(nome);
+console.log(nome.toUpperCase());
+console.log(nome.replace(' ', 'nardo '));
+console.log(nome.length);
+
+var btn = document.querySelector('.btn');
+console.log(btn);
+btn.style.color = "#8e0";
+
+btn.classList.add('ativo');
+
