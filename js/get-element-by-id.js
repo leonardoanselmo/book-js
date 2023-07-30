bdece97242bff6c709c4747366d045dce5816909
Léo Ansélmo <leonardo_anselmo@hotmail.com>
2023-07-30 18:09:30 -0300

    Adicionando um elemento ao HTML

--- js/get-element-by-id.js
+++ js/get-element-by-id.js
@@ -1,2 +1,18 @@
 const el = document.getElementById('one');
 el.className = 'cool';
+
+const newEl = document.createElement('li');
+newEl.className = 'cool';
+const newText = document.createTextNode('quinoa');
+
+newEl.appendChild(newText);
+
+const list = document.querySelector('ul');
+console.log(list);
+
+list.appendChild(newEl);
+
+// const position = document.getElementsByTagName('ul')[0];
+// console.log(position);
+// position.appendChild(newEl);
+
